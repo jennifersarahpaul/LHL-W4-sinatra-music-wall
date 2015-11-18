e695c938f98087971c568d6a24d5f6c0294c64e1
Jennifer Tigner <jennifertigner@email.com>
2015-11-18 05:03:00 +0000

    Added signups, signins, signouts

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,10 @@
 # THIS PAGE DEFINES THE LOGIC/ROUTES
+# How should I order this? Should I have all GETS together, all POSTS together?
+# Should post '/songs' do come right after get '/songs' do?
 
-DATABASE = {
-  'jennifertigner' => '1234'
-}
+# DATABASE = {
+#   'jennifertigner' => '1234'
+# }
 
 # LOGIN
 get '/' do
@@ -12,13 +14,15 @@ end
 
 get '/logout' do
   session.clear
+  session[:user_id] = nil
   redirect '/'
 end
 
 post '/login' do
   session['username_given'] = params['username']
-  userexist = DATABASE[params['username']]
-  if userexist && userexist == params['password'] # BCrypt::Password.new(userexist) == params['password']
+  user = User.find_by(username: params['username'])
+  if user && user.password == params['password'] 
+  # BCrypt::Password.new(userexist) == params['password']
     session['username'] = params['username']
     @songs = Song.all
     erb :'songs/index'
@@ -47,13 +51,34 @@ end
 
 post '/songs' do
   @song = Song.new(
-    artist:      params[:artist],
-    song_title:  params[:song_title],
-    url:         params[:url]
-  )
+    artist: params[:artist], song_title: params[:song_title], url: params[:url])
   if @song.save
     redirect '/songs'
   else
     erb :'songs/new'
+  end
+end
+
+post '/votes' do
+  vote = Vote.new(song_id: params[:song_id], user_id: params[:user_id])
+  if vote.save
+    redirect '/songs'
+  else
+    redirect '/songs?message=Cannot vote more than once'
+  end
+end
+
+get '/signup' do
+  @user = User.new
+  erb :signup
+end
+
+post '/signup' do
+  session['username_given_signup'] = params['username']
+  @user = User.new(username: params[:username], password: params[:password])
+  if @user.save
+    redirect '/songs'
+  else
+    erb :'/signup'
   end
 end
